19cb0ec1e277f6a393285430b9c02143908e5802
Sky "g3ner1c" Hong <g3ner1c-sky@pm.me>
2023-02-08 16:36:59 -0500

    cleaned up repo and migrated to the standard pyproject.toml with poetry

--- qbreader/__init__.py
+++ qbreader/__init__.py
@@ -1,1 +1,7 @@
+import importlib.metadata
+
 from .quizbowlAPI import query, random_question, random_name, packet, packet_bonuses, packet_tossups, num_packets, set_list, room_list, report_question
+
+__version__ = importlib.metadata.version("qbreader")
+
+del importlib
